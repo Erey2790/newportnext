28dd9f0cb2c021b84074b620bc3c808524e533fd
ElmerPC <erey2790@gmail.com>
2023-11-22 13:45:25 -0600

    mobile nav bar is now mobile operational

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   const handleNav = () => {
-
+      setNav(!nav)
   }
 
-  
+
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">Navbar
@@ -34,18 +34,20 @@ const Navbar = () => {
                 <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
             </Link>
         </ul>
-        <div className="md:hidden">
+        <div onClick={handleNav} className="md:hidden">
           <AiOutlineMenu size={25}/>
         </div>
       </div>
       </div>
 
-      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70': '' }>
+
+        <div className={nav ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
+        : 'fixed left-[-100%] top-0  p-10 ease-in duration-500'}>
           <div>
             <div className="flex w-full items-center justify-between">
               {/* logo here */}logo
-              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+              <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
                 <AiOutlineClose size={25} />
                 </div>
             </div>
