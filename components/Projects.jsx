d43a23f176978e73c02d35c49563d269d18f2efd
ElmerPC <erey2790@gmail.com>
2023-11-26 18:12:35 -0600

    adding a new component to make thing easier

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,8 @@
+import Image from 'next/image'
 import React from 'react'
+import nextjs from '../public/assets/nextjschatgpt.PNG'
+import Link from 'next/link'
+import ProjectItem from './ProjectItem'
 
 const Projects = () => {
   return (
@@ -6,7 +10,15 @@ const Projects = () => {
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
             <h2 className='py-4'>What I've Built</h2>
-            <div className='grid md:grid-cols-2 gap-8'></div>
+            <div className='grid md:grid-cols-2 gap-8'>
+
+                <ProjectItem title='ChatGPTClone' backgroundImg={nextjs} projectUrl='/property'/>
+                {/* <ProjectItem title='ChatGPTClone' backgroundImg={ChatGPTImage} projectUrl='/property'/>
+                <ProjectItem title='ChatGPTClone' backgroundImg={ChatGPTImage} projectUrl='/property'/>
+                <ProjectItem title='ChatGPTClone' backgroundImg={ChatGPTImage} projectUrl='/property'/> */}
+
+
+            </div>
         </div>
     </div>
   )
