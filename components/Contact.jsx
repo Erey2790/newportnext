01fe77cb21a0382f520eb1beec5a372af56476e2
ElmerPC <erey2790@gmail.com>
2023-11-30 14:07:32 -0600

    created contact button

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -68,6 +68,23 @@ const Contact = () => {
                                 <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
                             </div>
                         </div>
+
+                        <div className="flex flex-col py-2">
+                        <label className="uppercase text-sm py-2">Email</label>
+                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" />
+                        </div>
+
+                        <div className="flex flex-col py-2">
+                        <label className="uppercase text-sm py-2">Subject</label>
+                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
+                        </div>
+
+                        <div className="flex flex-col py-2">
+                        <label className="uppercase text-sm py-2">Message</label>
+                        <textarea className="border-2 rounded-lg p-3 flex border-gray-300" type="text" rows='10'> </textarea>
+                        </div>
+
+                        <button className="w-full p-4 text-gray-100 mt-4">Send Message</button>
                     </form>
                 </div>
             </div>
