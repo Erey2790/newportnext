2321d45b7988b07ca55da8fb1fffa4dcb6641ea5
ElmerPC <erey2790@gmail.com>
2023-12-01 18:27:21 -0600

    added route to chatgpt page

--- pages/chatgpt.jsx
+++ pages/chatgpt.jsx
@@ -1,8 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
+import chatgpt1 from '../public/assets/nextjschatgpt.PNG'
 
 const chatgpt = () => {
   return (
-    <div>chatgpt</div>
+    <div className='w-full'>
+      <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'/>
+        <Image className='absolute z-1' fill style={{objectFit:"cover"}} src={chatgpt1} alt='/' />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>ChatGPT Clone</h2>
+          <h3>NextJs / Tailwind / Firebase</h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>blah blah blah about the project</p>
+          <button className='px-8  py-2 mt-4 mr-8'>Demo</button>
+          <button className='px-8 py-2 mt-4'>Code</button>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
